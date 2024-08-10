4b853c7781353eeb4bdfda6843a5edfd792c511f
Gurpreet Singh <64939083+gurpreet1961@users.noreply.github.com>
2024-08-11 02:35:22 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,10 @@ const config = require("./config/config");
 const app = require("./app");
 
 let server;
-
+const port = config.port || 8082;
 mongoose.connect(config.mongoose.url).then(() => {
   console.log("Connected to MongoDB");
-  app.listen(config.port, () => {
+  app.listen(port, () => {
     console.log(`Server listening on port ${config.port}`);
   });
 });
